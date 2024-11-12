18c37cb66483069e180385ddfdc9fc70533c7bc1
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-11-12 20:12:01 +0000

    Updated scenes.py

--- scenes.py
+++ scenes.py
@@ -1,21 +1,28 @@
 # Module for scene functions.
 
 def game_intro():
+
     # Intro sequence.
     game_name = "Python Game"
     print(f"Welcome to {game_name}!")
     print("This is a game of choice.")
     print("You will be prompted when a choice needs to be made.")
+
     # Get player data.
-    name = input("What is your name, Player?")
+    name = input("What is your name, Player? ")
     start_hp = 50
     player = (name, start_hp)
+
     # Use name.
     print(f"Well, {name}, we hope you can solve the mystery!")
-    # Return player as an output.
+
+    # Return player data as an output.
     return player
 
 def scene1():
+
+    # Scene setting.
     print("The year is 1924.")
     print("You have been invited to the party of the year.")
+    
     return
