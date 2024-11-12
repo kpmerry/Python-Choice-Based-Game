d6b7b8dc50bb07dc244f57a7ddb251b6f14932e9
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-11-12 19:59:24 +0000

    created scenes and char modules for better readability

--- game.py
+++ game.py
@@ -1,7 +1,9 @@
 import time     # Will use to break up multiple print statements.
 import random
+import scenes   # Scene functions.
+import char     # Character information.
 
-class Person:
+class Player:
     # Set attributes.
     def __init__(self, name, health):
         self.name = name
@@ -9,31 +11,15 @@ class Person:
     # Set string representation.
     def __str__(self):
         return (f"{self.name}: {self.health}hp")
-
-def game_intro():
-    # Intro sequence.
-    game_name = "Python Game"
-    print(f"Welcome to {game_name}!")
-    print("This is a game of choice.")
-    print("You will be prompted when a choice needs to be made.")
-    # Get player data.
-    name = input("What is your name, Player?")
-    start_hp = 50
-    player = Person(name, start_hp)
-    # Use name.
-    print(f"Well, {player.name}, we hope you can solve the mystery!")
-    # Return player as an output.
-    return player
-
-def scene1():
-    print("The year is 1924.")
-    print("You have been invited to the party of the year.")
-    return
+    
+    def assign_player(info):
+        return Player(info[0],info[1])
 
 # Gameplay in the following section.
 
 # Introduction to game and set player.
-player = game_intro()
+player_info = scenes.game_intro()
+player = Player.assign_player(player_info)
 
 # Scene 1 with setting.
-scene1()
+scenes.scene1()
