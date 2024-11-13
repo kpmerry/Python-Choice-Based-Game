eb4c63f8bdf2ca7bd42ff3ec9844ef833ba3dbba
kpmerry <147255030+kpmerry@users.noreply.github.com>
2024-11-13 17:12:39 +0000

    filling in scenes

--- scenes.py
+++ scenes.py
@@ -1,12 +1,16 @@
 # Module for scene functions.
 
 import time
+import char
 
 def dialogue(character, dialogue):
     return None
 
-def slow_print(text):
-    speed = 0.5
+def input_choice(choices: tuple[str]):
+    return None
+
+def t_print(text: str):
+    speed = 0.8
     print(text)
     time.sleep(speed)
     return
@@ -15,9 +19,9 @@ def game_intro():
 
     # Intro sequence.
     game_name = "Python Game"
-    slow_print(f"Welcome to {game_name}!")
-    slow_print("This is a game of choice.")
-    slow_print("You will be prompted when a choice needs to be made.")
+    t_print(f"Welcome to {game_name}!")
+    t_print("This is a game of choice.")
+    t_print("You will be prompted when a choice needs to be made.")
 
     # Get player data.
     name = input("What is your name, Player? ")
@@ -25,7 +29,7 @@ def game_intro():
     player = (name, start_hp)
 
     # Use name.
-    slow_print(f"Well, {name}, we hope you can solve the mystery!")
+    t_print(f"Well, {name}, we hope you can solve the mystery!")
 
     # Return player data as an output.
     return player
@@ -33,14 +37,29 @@ def game_intro():
 def scene1():
 
     # Scene setting.
-    slow_print("The year is 1924.")
-    slow_print("You have been invited to the party of the year.")
+    t_print("The year is 1924.")
+    t_print("You have been invited to the party of the year.")
     
     return
 
 def scene2():
+    """Details a players visit to murder scene."""
 
-    #Scene setting.
-    slow_print("You approach the scene.")
+    # Scene setting.
+    # May need separate dialogue file for bulkier scenes.
+    t_print("You approach the scene.")
+    t_print("The cold night air whips around you.")
 
     return
+
+def scene3():
+    return None
+
+def scene4():
+    return None
+
+def scene5():
+    return None
+
+def scene6():
+    return None
